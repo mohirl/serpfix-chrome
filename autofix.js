1d22d1cb515e68b7adffbcd28667c9b1ef80a72d
Mick O'Hea <mick.ohea@gmail.com>
2014-03-12 23:04:04 +0000

    Version 0.1

--- autofix.js
+++ autofix.js
@@ -1,3 +1,13 @@
-$.get(chrome.extension.getURL("statusbar.html"), {}, function(data) {$('body').append(data);}, 'html');
-// var css = '#res h3 {font-size: medium!important;} #tads a, #tadsb a, #res a, #rhs a, #taw a {text-decoration: underline!important;';
-   chrome.tabs.insertCSS({code: 'fix.css'});
+chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+
+     // since only one tab should be active and in the current window at once
+     // the return variable should only have one entry
+     var activeTab = arrayOfTabs[0];
+     var activeTabUrl = arrayOfTabs[0].url; // or do whatever you need
+     
+     // Check the url contains q=, indicating a google search query, and if so inject the CSS
+     if (activeTabUrl.indexOf("q=") > -1)
+        chrome.tabs.insertCSS({code: 'fix.css'});
+
+
+  });
